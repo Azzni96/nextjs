8fde8074e349de198bda3530d3bccae9c3b27453
Your Name <you@example.com>
2026-01-17 09:53:07 +0200

    Task2: add about page

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>About Us</h1>
+            <p>Welcome to our application! We are dedicated to providing the best service possible.</p>
+            <p>Our team is passionate about technology and innovation, striving to create solutions that make a difference.</p>
+            <p>Thank you for visiting our about page. We hope to connect with you soon!</p>
+        </main>
+    );
+}
