cf61578c8e795a8c23ac556f47b0ae81f54ede13
Your Name <you@example.com>
2026-01-17 10:18:41 +0200

    Task3: add navigation links between home and about

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function AboutPage() {
     return (
         <main>
@@ -5,6 +6,8 @@ export default function AboutPage() {
             <p>Welcome to our application! We are dedicated to providing the best service possible.</p>
             <p>Our team is passionate about technology and innovation, striving to create solutions that make a difference.</p>
             <p>Thank you for visiting our about page. We hope to connect with you soon!</p>
+
+            <Link href="/">Back to Home Page</Link>
         </main>
     );
 }
