552a7894d61a5831177e7e93c965b8c3b6575402
Your Name <you@example.com>
2026-01-17 10:35:37 +0200

    Task5: Add style for home page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import style from './home/home.module.css';
 
 export default function HomePage() {
   return (
     <main>
-      <h1>My Next.js Learning App</h1>
-      <p>
+      <h1 className={style.title}>My Next.js Learning App</h1>
+      <p className={style.text}>
         This project is for practicing Next.js basics: pages, routing, and
         navigation.
       </p>
 
-      <Link href="/about">Go to About Page</Link>
+      <Link href="/about" className={style.link}>Go to About Page</Link>
     </main>
   );
 }
