e542f8bde306509bee87e5a7ce5dc0288a865ea1
Your Name <you@example.com>
2026-01-17 10:29:20 +0200

    Task4: personalize home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 
 export default function HomePage() {
-    return (
-        <main>
-            <h1>Welcome to Our Application</h1>
-            <p>This is the home page of our Next.js application.</p>
-            <Link href="/about">Go to About Page</Link>
-        </main>
-    );
-} 
+  return (
+    <main>
+      <h1>My Next.js Learning App</h1>
+      <p>
+        This project is for practicing Next.js basics: pages, routing, and
+        navigation.
+      </p>
+
+      <Link href="/about">Go to About Page</Link>
+    </main>
+  );
+}
