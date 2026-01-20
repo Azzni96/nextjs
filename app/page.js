87607ca33e3f9e215717f42aa371f8bc42899a89
Your Name <you@example.com>
2026-01-20 19:46:15 +0200

    Styling Task 1: use Title component on home page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import styles from "./home/home.module.css";
 import Navigation from "./components/Navigation";
+import Title from "./components/Title";
 
 export default function HomePage() {
   return (
     <main>
       <Navigation />
 
-      <h1 className={styles.title}>My Next.js Learning App</h1>
+      <Title>Styling Assignment 1</Title>
+
       <p className={styles.text}>
         This project is for practicing Next.js basics: pages, routing, navigation,
         and styling with CSS Modules.
